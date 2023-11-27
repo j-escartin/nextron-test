56b8f596b30895d0ee81306a46be0dc1d6fce1fc
j-escartin <javines2403@gmail.com>
2023-11-27 14:53:47 +0800

    added accordion and calendar

--- renderer/pages/next.tsx
+++ renderer/pages/next.tsx
@@ -2,8 +2,16 @@ import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
+import { Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '../@/components/ui/accordion'
+import { Calendar } from '../@/components/ui/calendar'
+
 
 export default function NextPage() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date())
   return (
     <React.Fragment>
       <Head>
@@ -26,6 +34,34 @@ export default function NextPage() {
           <a className="btn-blue">Go to home page</a>
         </Link>
       </div>
+      <Accordion type="single" collapsible className="w-full">
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger>Is it styled?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It comes with default styles that matches the other
+          components&apos; aesthetic.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger>Is it animated?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It&apos;s animated by default, but you can disable it if you
+          prefer.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+    <Calendar
+    mode="single"
+    selected={date}
+    onSelect={setDate}
+    className="rounded-md border"
+    />
     </React.Fragment>
   )
 }
